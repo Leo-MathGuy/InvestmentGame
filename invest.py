4a8c308d2c2286a7734d308a58938df9022dbfcf
Leo-MathGuy <leonidiudakov@gmail.com>
2021-10-17 13:52:27 -0700

    Added docs, changing banks, and expanded ask function

--- invest.py
+++ invest.py
@@ -1,6 +1,18 @@
 # By Leo-MathGuy
 # You can copy this code if you don't sell it, attribute it to me, and share it with this same licence.
 
+"""
+Code order:
+
+Line function
+Gamemode
+Ask
+Ask control
+Banks
+Tutorial
+Main game
+Beginning
+"""
 from math import *
 from time import sleep
 
@@ -11,6 +23,10 @@ print("Welcome to the investment game! \n\n")
 day = 1
 money = 0
 
+# Beatiful seperator
+
+def line(x):
+	print("-"*x)
 # Game Modes
 class gamemode:
 	def __init__(self, name, advanced, startmoney, goal):
@@ -18,6 +34,8 @@ class gamemode:
 		self.advanced = False #Disabled for now
 		self.start = startmoney
 		self.goal = goal * 10000 #To avoid unnecessary 0s
+
+################################################################################################################################
 
 # Things that start with a g before a word are linked to the gamemode
 gveasy = gamemode("Very Easy", False, 250, 50)
@@ -27,7 +45,9 @@ ghard = gamemode("Hard", False, 75, 150)
 gvhard = gamemode("Very Hard", True, 50, 200)
 gbrut = gamemode("Brutal", True, 30, 500)
 
-def ask(q, h,  help="True", **kwargs):  #Universal ask function. Provide quesiton, help, and kwargs as option:return
+################################################################################################################################
+
+def ask(q, h,  help=True, **kwargs):  #Universal ask function. Provide quesiton, help, and kwargs as option:return
 	optionl = list(kwargs.keys())
 	options = ""
 	for i in range(0, len(optionl)):
@@ -44,11 +64,15 @@ def ask(q, h,  help="True", **kwargs):  #Universal ask function. Provide quesito
 		if inpt == "help" and help:
 			print(h)
 		else:
-			if inpt in optionl:
-				sleep(0.2)
-				return kwargs[inpt]
+			for item in optionl:
+				if inpt.title() == item:
+					sleep(0.2)
+					return kwargs[inpt.title()]
+		
 			sleep(0.35)
 			print("Error: Unknown Input. Please enter valid option\n")
+
+################################################################################################################################
 
 whatDo = "What do you want to do?"
 
@@ -58,10 +82,18 @@ class askControl:
 	
 	def mainLoop():
 		opts = {"Invest": "I", "Banks": "B", "Wait":"W", "Stats":"S", "Settings":"X", "Exit":"E"}
-		return ask(whatDo, "Option = Action", **opts)
+		return ask(whatDo, "Option = Action. You do not need to capitaliaze", **opts)
 	def bank():
 		opts = {"Details": "D", "Change Bank": "C", "Exit": "E"}
 		return ask(whatDo, "", False, **opts)
+	def bankTwo():
+		opts = {"Join Bank": "J", "Next Bank": "N", "Exit": "E"}
+		return ask(whatDo, "", False, **opts)
+
+
+################################################################################################################################
+
+
 # Banks
 
 class bank:
@@ -79,37 +111,87 @@ bankList = [
 cBank = 0
 bankObj = bankList[cBank]
 
+
 def banks():
+	global money
+	global bankObj
+	global cBank
+	
 	print(f"Current bank: {bankList[cBank].name}")
 	print(f"Balance: {money}")
 	while True:
 		option = askControl.bank()
+		line(50)
 		match option:
 			case "E":
 				return
 			case "D":
-				print("-" * 20)
+				line(20)
 				print(f"Bank: {bankObj.name}")
 				print(f"Description: {bankObj.description}")
 				print(f"Maximum credit: {bankObj.maxCredit}")
 				print(f"Max balace: {bankObj.maxBalance}")
-				print("-" * 20)
+				line(20)
 			case "C":
-				print("Work In Progress")
+				if cBank+1 == len(bankList):
+					print("Sorry, this is the final bank")
+					line(69)
+				else:
+					skipped = False
+					i = 0
+					while True:
+						i += 1
+						nextBank = bankList[cBank+i]
+						if not skipped:
+							print("\nNext bank:")
+							print(f"Name: {nextBank.name}")
+							print(f"Description: {nextBank.description}")
+							print(f"Joining Fee: {nextBank.joinFee}")
+							print(f"Max Balance: {nextBank.maxBalance}")
+							print(f"Max Credit: {nextBank.maxCredit}\n")
+							option = askControl.bankTwo()
+						skipped = False
+						match option:
+							case "N":
+								if cBank+i+1 == len(bankList):
+									print("\nSorry, this is the final bank")
+									i -= 2
+									skipped = True
+								else:
+									continue
+							case "J":
+								if nextBank.joinFee <= money:
+									cBank += i
+									bankObj = nextBank
+									money -= nextBank.joinFee
+									line(15)
+									print("\nBank joined!\n")
+									line(15)
+									break
+							case "E":
+								break
+
+################################################################################################################################
+
 #Tutorial
 def tutorial():
 	print("===Work=In=Progress===(tutorial)")
 
+################################################################################################################################
+
 ### Main Game Loop ###
 def main_game():
 	while True:
-		print("-" * 69)
+		line(69)
 		print(f"Day {day}\nMoney: {money}")
 		option = askControl.mainLoop()
 		if option == "E":
 			break
 		elif option == "B":
+			line(69)
 			banks()
+
+################################################################################################################################
 
 ##Beginning##
 
@@ -128,15 +210,15 @@ if gadv: var = "On"
 else: var = "Off"
 
 ginit_dict = {"Name: ":gname, "Starting Money: $":gstart, "Goal: $":goal, "Advanced Mode: ":var}
-sleep(0.25)
-print("\n")
+
 for item in list(ginit_dict.keys()):
 	sleep(0.25)
 	print(item, end='')
 	print(str(ginit_dict[item])+"\n")
 sleep(0.25)
 
-if ask("Do you want a tutorial?", "y = begin tutorial  n = skip tutorial", y=True, n=False):
+if ask("Do you want a tutorial?", False, Yes=True, No=False):
 	tutorial()
 else:
 	main_game()
+main_game()
